27762189e7cf269903de65278ed84fd5d5010864
Matthew Carter <m@ahungry.com>
2019-01-02 22:06:31 -0500

    First

---
+++ first.scm
@@ -0,0 +1,14 @@
+;; I am a comment
+
+#|
+Multi line comment, nice.
+|#
+
+(define hw (lambda ()
+             (display "Hw")
+             (newline)))
+
+(define (hello-name n)
+  (display "Hello ")
+  (display n)
+  (newline))
